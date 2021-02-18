05088861d790d7e01df1b904784a63f88cfdd752
dydysy <200615@gmail.com>
2021-02-18 22:07:52 +0800

    first commit

---
+++ lib/btc_addr.ex
@@ -0,0 +1,18 @@
+defmodule BtcAddr do
+  @moduledoc """
+  Documentation for `BtcAddr`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BtcAddr.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/btc_addr_test.exs
@@ -0,0 +1,8 @@
+defmodule BtcAddrTest do
+  use ExUnit.Case
+  doctest BtcAddr
+
+  test "greets the world" do
+    assert BtcAddr.hello() == :world
+  end
+end
